48ed701cb5327ace89262dd76640c9def21c4bcb
trhoangdung <trhoangdung@gmail.com>
2017-12-12 18:26:34 -0600

    modify plot error versus time-step

--- examples/plot_err_vs_timestep.py
+++ examples/plot_err_vs_timestep.py
@@ -6,6 +6,7 @@ Dung Tran: 12/2017
 import matplotlib.pyplot as plt
 from engine.fem import Fem1D
 from engine.verifier import ReachSetAssembler
+import matplotlib.patches as mpatches
 from engine.plot import Plot
 import numpy as np
 
@@ -22,8 +23,8 @@ if __name__ == '__main__':
     print "\nmesh_points = {}".format(mesh_points)
     x = 8.0
     x_ind = int(x / 0.5) - 1
-    steps = [0.2, 0.1, 0.05]    # time step of FEM
-    colors = ['r', 'm', 'g']
+    steps = [0.1, 0.05]    # time step of FEM
+    colors = ['r', 'g']
 
     fig3 = plt.figure()
     ax3 = fig3.add_subplot(111)
@@ -47,24 +48,19 @@ if __name__ == '__main__':
         ############################################################
         # compute error dicrete reachable set
         RSA = ReachSetAssembler()
-        _, e_dset, _ = RSA.get_dreachset(dPde, toTimeStep)    # compute discrete reachable set
-        e_lines_at_x_8_list = []
-        for i in xrange(0, toTimeStep + 1):
-            e_lines_at_x_8, _, _, _, _ = e_dset[i].get_lines_set()
-            e_lines_at_x_8_list.append(e_lines_at_x_8[x_ind])
+        _, e_inspace, _, _, _, _ = RSA.get_interpolationset(dPde, toTimeStep)
+        e_boxes = e_inspace[toTimeStep].get_2D_boxes(alpha_range, beta_range)
+        ax3 = pl3.plot_boxes(ax3, e_boxes, facecolor=colors[j], edgecolor=colors[j])
 
-        ############################################################
-        # Plot dreach set of e at x = 8
-
-        ax3 = pl3.plot_vlines(ax3, time_list.tolist(), e_lines_at_x_8_list, colors=colors[j], linestyles='solid')
-
-    ax3.legend([r'$k = {}$'.format(steps[0]), r'$k = {}$'.format(steps[1]), r'$k = {}$'.format(steps[2])])
-    ax3.set_ylim(0, 0.9)
-    ax3.set_xlim(-0.2, 10.5)
+    ax3.set_ylim(0.0, 0.4)
+    ax3.set_xlim(0, 10.5)
     plt.xticks(fontsize=20)
     plt.yticks(fontsize=20)
-    plt.xlabel('$t$', fontsize=20)
-    plt.ylabel(r'$\tilde{}(x={},t)$'.format('e', x), fontsize=20)
-    fig3.suptitle('Error Vs. Time-Step', fontsize=25)
-    fig3.savefig('err_vs_timestep.pdf')
+    plt.xlabel('$x$', fontsize=20)
+    plt.ylabel(r'$\bar{e}(x,t=10s)$', fontsize=20)
+    red_patch = mpatches.Patch(color='r', label='$k = 0.1$')
+    green_patch = mpatches.Patch(color='g', label='$k = 0.05$')
+    plt.legend(handles=[red_patch, green_patch])
+    fig3.suptitle('Error Vs. Space-Step at $t=10s$', fontsize=25)
+    fig3.savefig('err_vs_time_step.pdf')
     plt.show()
