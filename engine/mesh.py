adcee98ac4d622c45ef63f6d254cab29ccbcb5fb
trhoangdung <trhoangdung@gmail.com>
2018-02-21 10:47:44 -0600

    finish get_edges_mat method

--- engine/mesh.py
+++ engine/mesh.py
@@ -25,7 +25,8 @@ class Triangulation_2D(object):
 
         self.edges_mat = None    # edges matrix
         self.num_edges = None    # number of edges
-        self.bd_edges_mat = None    # number of boundary edges
+        self.bd_edges_mat = None    # boundary edges matrix
+        self.num_bd_edges = None    # number of boundary edges
 
     def get_edges_mat(self):
         'compute edges and boundary edges matrix'
@@ -48,21 +49,30 @@ class Triangulation_2D(object):
 
         I, J, V = find(s_mat)
         self.edges_mat = np.transpose(np.array([I[:], J[:]]))
+        self.num_edges = V.shape[0]
         print "\nedges matrix = \n{}".format(self.edges_mat)
-        num_bd_edges = 0
+        print "\nnumber of edges is : {}".format(self.num_edges)
+        bd_I = []
+        bd_J = []
         for i in xrange(0, V.shape[0]):
             if V[i] == 1:
-                num_bd_edges += 1
+                bd_I.append(I[i])
+                bd_J.append(J[i])
 
-
-
+        bd_I = np.asarray(bd_I)
+        bd_J = np.asarray(bd_J)
+        self.bd_edges_mat = np.transpose(np.array([bd_I[:], bd_J[:]]))
+        self.num_bd_edges = bd_I.shape[0]
+        print "\nboundary edges matrix = \n{}".format(self.bd_edges_mat)
+        print "\nnumber of boundary edges is : {}".format(self.num_bd_edges)
+        print "\nnumber of interior edges is : {}".format(self.num_edges - self.num_bd_edges)
 
 
 def test():
     'test mesh.py'
 
     nodes = np.array([[1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1], [0, 0]])
-    elements = np.array([[1, 2, 8], [3, 8, 2], [8, 3, 5], [4, 5, 3], [7, 8, 6], [5, 6, 8]])
+    elements = np.array([[0, 1, 7], [2, 7, 1], [7, 2, 4], [3, 4, 2], [6, 7, 5], [4, 5, 7]])
 
     mesh = Triangulation_2D(nodes, elements)
 
